1ff4026ec8d8a7fc9e1df5a2bb2a36ab19c17480
liuyongqing <ly862695473@163.com>
2023-04-19 21:52:05 +0800

    ✨ feat: 删除多余代码

--- src/components/Layout/components/Tabs/index.tsx
+++ src/components/Layout/components/Tabs/index.tsx
@@ -35,11 +35,12 @@ const LayoutTabs = () => {
 			tabsList.forEach((item: Menu.MenuOptions, index: number) => {
 				if (item.path !== pathname) return;
 				const nextTab:Menu.MenuOptions = tabsList[index + 1] || tabsList[index - 1];
-        console.log(nextTab);
 				if (!nextTab) return;
 				useNavigateTo(nextTab.path)  ;
 			});
 		}
+   
+    
 		setTabsList(tabsList.filter((item: Menu.MenuOptions) => item.path !== tabPath))
   };
   return (
