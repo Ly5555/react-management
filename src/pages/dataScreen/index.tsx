93677d0a08a662cae77deb09a74b7d2188397f59
liuyongqing <>
2023-06-20 21:48:18 +0800

    1

--- src/pages/dataScreen/index.tsx
+++ src/pages/dataScreen/index.tsx
@@ -1,103 +1,64 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import dayjs from "dayjs";
-import {Tabs, Table} from "antd";
-import type {TabsProps} from "antd";
+import { Tabs, Table } from "antd";
+import type { TabsProps } from "antd";
 import styles from "./css/index.module.less";
 
 const DataScreen = () => {
-  const dataSource1:any = [
-    {
-      origOrderNo: "498123",
-      goodsList: [
-        {
-          name: "测试商品",
-          code: "7148724",
-          unitPrice: "2",
-          count: 1,
-        },
-        {
-          name: "休闲运动包",
-          code: "13024",
-          unitPrice: "2",
-          count: 1,
-        },
-      ],
-      freeFlag: "开启",
-    },
-    {
-      origOrderNo: "505775",
-      goodsList: [
-        {
-          itemName: "测试商品",
-          itemCode: "7148724",
-          unitPrice: "2",
-          count: 1,
-        },
-      ],
-      freeFlag: "关闭",
-    },
-  ];
-  const columns1:any = [
-    {
-      title: "姓名",
-      dataIndex: "origOrderNo",
-    },
-    {
-      title: "年龄",
-      dataIndex: "goodsList",
-    },
-    {
-      title: "住址",
-      dataIndex: "freeFlag",
-    },
-  ];
   const dataSource = [
     {
-      origOrderNo: "498123",
-      goodsList: [
-        {
-          name: "测试商品",
-          code: "7148724",
-          unitPrice: "2",
-          count: 1,
-        },
-        {
-          name: "休闲运动包",
-          code: "13024",
-          unitPrice: "2",
-          count: 1,
-        },
-      ],
+      name: "home",
+      age: "12",
       freeFlag: "开启",
     },
     {
-      origOrderNo: "505775",
-      goodsList: [
-        {
-          itemName: "测试商品",
-          itemCode: "7148724",
-          unitPrice: "2",
-          count: 1,
-        },
-      ],
+      name: "home",
+      age: "22",
       freeFlag: "关闭",
     },
   ];
-  
+  const getRowSpan = (text: any, record: any, index: any, data: any, dataIndex: any) => {
+    const count = {} as any
+    data.forEach(item => {
+      const key = item[dataIndex]
+      if (count.hasOwnProperty(key)) {
+        count[key]++
+      } else {
+        count[key] = 1
+      }
+    })
+    const rowSpan = count[text]
+    if (index > 0 && record[dataIndex] === data[index - 1][dataIndex]) {
+      return 0
+    } else {
+      return rowSpan
+    }
+  }
   const columns = [
     {
       title: "姓名",
-      dataIndex: "origOrderNo",
+      dataIndex: "name",
+      onCell: (text: any, record: any, index: any) => {
+        const isFirstRow = index === 0; // 判断是否是第一行
+        const rowSpan = isFirstRow ? 2 : 0; // 合并两行
+        return {
+          children: text,
+          props: {
+            rowSpan: rowSpan,
+          },
+        };
+      }
     },
     {
       title: "年龄",
-      dataIndex: "goodsList.name",
+      dataIndex: "age",
     },
     {
       title: "住址",
       dataIndex: "freeFlag",
     },
   ];
+
   return (
     <div className={styles.dataScreen}>
       <div className={styles.dataScreen_h2}>数据可视化</div>
