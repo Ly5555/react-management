4255b420bbc3d25291bf3177309e6fada996500d
liuyongqing <>
2023-10-11 21:55:46 +0800

    ✨ feat: 合并单元格跨页

--- src/pages/FormTable/index.tsx
+++ src/pages/FormTable/index.tsx
@@ -2,15 +2,13 @@
  * @Author: liuyongqing
  * @Date: 2023-08-30 20:30:01
  * @LastEditors: liuyongqing
- * @LastEditTime: 2023-10-07 10:55:59
+ * @LastEditTime: 2023-10-11 21:54:13
  */
 import React, { useState, useEffect } from "react";
 import { Button, Col, Form, Input, Row, Select, Space, Table } from "antd";
 import { BatchRejectionModal } from "./components/index";
 import { DownOutlined, UpOutlined } from "@ant-design/icons";
-import { ColumnsType } from "antd/es/table/interface";
 import request from "@/utils/request/request";
-import { LoginForm } from "../LoginPage/components";
 const SearchForm = () => {
   const [expand, setExpand] = useState(false);
   const [form] = Form.useForm();
@@ -84,11 +82,8 @@ const SearchForm = () => {
 // form 表单封装初体验
 const Index = () => {
   const [selectedRowKeys, setSelectedRowKeys] = useState([]);
-  const [dataSource, setDataSource] = useState([]);
-  const [pagination, setPagination] = useState({
-    current: 1,
-    pageSize: 10,
-  });
+  const [rowspanArray, setRowspanArray] = useState([]);
+  const [pagination, setPagination] = useState({ current: 1, pageSize: 10 });
   useEffect(() => {
     getData();
   }, []);
@@ -98,359 +93,198 @@ const Index = () => {
       method: "post",
     });
     const { dataList } = data || [];
-    setDataSource(dataList);
   };
-  // const data = [
-  //   {
-  //     key: "11",
-  //     name: "John Brown",
-  //     age: 32,
-  //     tel: "0571-22098909",
-  //     phone: 18889898989,
-  //     address: "New York No. 1 Lake Park",
-  //   },
-  //   {
-  //     key: "12",
-  //     name: "John Brown",
-  //     age: 32,
-  //     tel: "0571-22098909",
-  //     phone: 18889898989,
-  //     address: "New York No. 1 Lake Park",
-  //   },
-  //   {
-  //     key: "13",
-  //     name: "John Brown",
-  //     age: 32,
-  //     tel: "0571-22098909",
-  //     phone: 18889898989,
-  //     address: "New York No. 1 Lake Park",
-  //   },
-  //   {
-  //     key: "14",
-  //     name: "John Brown",
-  //     age: 32,
-  //     tel: "0571-22098909",
-  //     phone: 18889898989,
-  //     address: "New York No. 1 Lake Park",
-  //   },
-  //   {
-  //     key: "1",
-  //     name: "John Brown",
-  //     age: 32,
-  //     tel: "0571-22098909",
-  //     phone: 18889898989,
-  //     address: "New York No. 1 Lake Park",
-  //   },
-  //   {
-  //     key: "15",
-  //     name: "John Brown",
-  //     age: 32,
-  //     tel: "0571-22098909",
-  //     phone: 18889898989,
-  //     address: "New York No. 1 Lake Park",
-  //   },
-  //   {
-  //     key: "16",
-  //     name: "John Brown",
-  //     age: 32,
-  //     tel: "0571-22098909",
-  //     phone: 18889898989,
-  //     address: "New York No. 1 Lake Park",
-  //   },
-  //   {
-  //     key: "2",
-  //     name: "Jim Green",
-  //     tel: "0571-22098333",
-  //     phone: 18889898888,
-  //     age: 42,
-  //     address: "London No. 1 Lake Park",
-  //   },
-  //   {
-  //     key: "3",
-  //     name: "Joe Black",
-  //     age: 32,
-  //     tel: "0575-22098909",
-  //     phone: 18900010002,
-  //     address: "Sidney No. 1 Lake Park",
-  //   },
-  //   {
-  //     key: "4",
-  //     name: "Jim Red",
-  //     age: 18,
-  //     tel: "0575-22098909",
-  //     phone: 18900010002,
-  //     address: "London No. 2 Lake Park",
-  //   },
-  //   {
-  //     key: "51",
-  //     name: "Jake White",
-  //     age: 18,
-  //     tel: "0575-22098909",
-  //     phone: 18900010002,
-  //     address: "Dublin No. 2 Lake Park",
-  //   },
-  //   {
-  //     key: "53",
-  //     name: "Jake White",
-  //     age: 18,
-  //     tel: "0575-22098909",
-  //     phone: 18900010002,
-  //     address: "Dublin No. 2 Lake Park",
-  //   },
-  //   {
-  //     key: "5",
-  //     name: "Jake White",
-  //     age: 18,
-  //     tel: "0575-22098909",
-  //     phone: 18900010002,
-  //     address: "Dublin No. 2 Lake Park",
-  //   },
-  // ];
-  const originData = [
-    { id: 1, phone: 15800000000, name: "杨", info: "端口错误", time: "2022-12-12" },
-    { id: 2, phone: 15800000000, name: "杨", info: "网络错误", time: "2022-12-13" },
-    { id: 3, phone: 15800000000, name: "杨", info: "操作错误", time: "2022-12-14" },
-    { id: 4, phone: 13600000000, name: "张", info: "端口错误", time: "2022-12-12" },
-    { id: 5, phone: 13600000000, name: "张", info: "操作错误", time: "2022-12-13" },
-    { id: 6, phone: 17700000000, name: "李", info: "端口错误", time: "2022-12-12" },
-  ];
-  const orderArr: number[] = [];
-  originData.forEach((item) => {
-    if (orderArr.includes(item.phone) === false) {
-      orderArr.push(item.phone);
-    }
-  });
-
-  const infoMap = new Map();
-  originData.forEach((item, index) => {
-    const nowItem = infoMap.get(item.phone);
-    if (nowItem) {
-      nowItem.ids.push(item.id);
-      infoMap.set(item.phone, nowItem);
+  const renderTel = (text, row, index, rowKey) => {
+    // console.log(text, "98");
+    const { current, pageSize } = pagination;
+    const obj = {
+      children: text,
+      props: {},
+    };
+    let sameRowCount = 1;
+    let totalIndex = pageSize * current;
+    totalIndex = totalIndex > data.length ? data.length : totalIndex;
+    const fullIndex = pageSize * (current - 1) + index;
+    if (index !== 0 && data[fullIndex - 1][rowKey] === data[fullIndex][rowKey]) {
+      sameRowCount = 0;
     } else {
-      infoMap.set(item.phone, { startIndex: index, ids: [item.id] });
-    }
-  });
-  const firstKeyArr = [];
-  infoMap.forEach((value) => {
-    firstKeyArr.push(value.startIndex);
-  });
-  const handleOnCell = (data: any, index: number | undefined) => {
-    const infoItem = infoMap.get(data.phone);
-    if (infoItem) {
-      return { rowSpan: index === infoItem.startIndex ? infoItem.ids.length : 0 };
-    }
-    return { rowSpan: 1 };
-  };
-  const columns: any = [
-    {
-      title: "排序",
-      dataIndex: "order",
-      render: (text) => text + 1,
-      onCell: handleOnCell,
-    },
-    {
-      title: "电话",
-      dataIndex: "phone",
-      onCell: handleOnCell,
-    },
-    {
-      title: "姓名",
-      dataIndex: "name",
-      onCell: handleOnCell,
-    },
-    {
-      title: "信息",
-      dataIndex: "info",
-      width: "20%",
-    },
-    {
-      title: "时间",
-      dataIndex: "time",
-    },
-  ];
-  const data = originData.map((item) => ({
-    ...item,
-    order: orderArr.indexOf(item.phone),
-  }));
-
-  const handleOnSelect = (record: any, selected: boolean, selectedRows: any) => {
-    const infoItem = infoMap.get(record.phone);
-    console.log(selectedRows);
-
-    let newSelectedRowKeys = selectedRows.map((item: any) => item.id);
-    console.log(newSelectedRowKeys);
-    if (infoItem) {
-      if (selected) {
-        newSelectedRowKeys.push(...infoItem.ids);
-      } else {
-        newSelectedRowKeys = newSelectedRowKeys.filter((item: any) => infoItem.ids.includes(item as number) === false);
+      for (let i = fullIndex + 1; i < totalIndex; i++) {
+        if (data[i][rowKey] === data[fullIndex][rowKey]) {
+          sameRowCount++;
+        } else {
+          break;
+        }
       }
     }
-
-    setSelectedRowKeys(newSelectedRowKeys);
+    obj.props.rowSpan = sameRowCount;
+    return obj;
   };
-
-  const handleOnSelectAll = (selected: any, selectedRows: any, changeRows: any) => {
-    setSelectedRowKeys(selected ? data.map((item: any) => item.id) : []);
+  const renderTel2 = (record, index, rowKey) => {
+    const { current, pageSize } = pagination;
+    let obj = {
+      rowSpan: 0,
+    };
+    let sameRowCount = 1;
+    let totalIndex = pageSize * current;
+    totalIndex = totalIndex > data.length ? data.length : totalIndex;
+    const fullIndex = pageSize * (current - 1) + index;
+    if (index !== 0 && data[fullIndex - 1][rowKey] === data[fullIndex][rowKey]) {
+      sameRowCount = 0;
+    } else {
+      for (let i = fullIndex + 1; i < totalIndex; i++) {
+        if (data[i][rowKey] === data[fullIndex][rowKey]) {
+          sameRowCount++;
+        } else {
+          break;
+        }
+      }
+    }
+    obj.rowSpan = sameRowCount;
+    return obj;
   };
-  console.log(selectedRowKeys);
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      render: (text, record, index) => {
+        return <a href="#">{text}</a>;
+      },
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      render: (text, row, index) => renderTel(text, row, index, "age"),
+    },
+    {
+      title: "Home phone",
+      colSpan: 2,
+      dataIndex: "tel",
+      onCell: (record, rowIndex) => renderTel2(record, rowIndex, "tel"),
+    },
+    {
+      title: "Phone",
+      colSpan: 0,
+      dataIndex: "phone",
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+    },
+  ];
 
-  // const columns: ColumnsType = [
-  //   {
-  //     title: "姓名",
-  //     dataIndex: "name",
-  //   },
-  //   {
-  //     title: "年龄",
-  //     dataIndex: "age",
-  //   },
-  //   {
-  //     title: "住址",
-  //     dataIndex: "address",
-  //   },
-  //   {
-  //     title: "Home phone",
-  //     colSpan: 2,
-  //     dataIndex: "tel",
-  //     render: (value, row, index) => {
-  //       const { current, pageSize } = pagination || {};
-  //       const obj = {
-  //         children: value,
-  //         props: {},
-  //       };
-  //       let sameRowCount = 1;
+  const data: any = [
+    {
+      key: "11",
+      name: "John Brown",
+      age: 32,
+      tel: "0571-22098909",
+      phone: 18889898989,
+      address: "New York No. 1 Lake Park",
+    },
+    {
+      key: "12",
+      name: "John Brown",
+      age: 32,
+      tel: "0571-22098909",
+      phone: 18889898989,
+      address: "New York No. 1 Lake Park",
+    },
+    {
+      key: "13",
+      name: "John Brown",
+      age: 32,
+      tel: "0571-22098909",
+      phone: 18889898989,
+      address: "New York No. 1 Lake Park",
+    },
+    {
+      key: "14",
+      name: "John Brown",
+      age: 32,
+      tel: "0571-22098909",
+      phone: 18889898989,
+      address: "New York No. 1 Lake Park",
+    },
+    {
+      key: "1",
+      name: "John Brown",
+      age: 32,
+      tel: "0571-22098909",
+      phone: 18889898989,
+      address: "New York No. 1 Lake Park",
+    },
+    {
+      key: "15",
+      name: "John Brown",
+      age: 32,
+      tel: "0571-22098909",
+      phone: 18889898989,
+      address: "New York No. 1 Lake Park",
+    },
+    {
+      key: "16",
+      name: "John Brown",
+      age: 32,
+      tel: "0571-22098909",
+      phone: 18889898989,
+      address: "New York No. 1 Lake Park",
+    },
+    {
+      key: "2",
+      name: "Jim Green",
+      tel: "0571-22098333",
+      phone: 18889898888,
+      age: 42,
+      address: "London No. 1 Lake Park",
+    },
+    {
+      key: "3",
+      name: "Joe Black",
+      age: 32,
+      tel: "0575-22098909",
+      phone: 18900010002,
+      address: "Sidney No. 1 Lake Park",
+    },
+    {
+      key: "4",
+      name: "Jim Red",
+      age: 18,
+      tel: "0575-22098909",
+      phone: 18900010002,
+      address: "London No. 2 Lake Park",
+    },
+    {
+      key: "51",
+      name: "Jake White",
+      age: 18,
+      tel: "0575-22098909",
+      phone: 18900010002,
+      address: "Dublin No. 2 Lake Park",
+    },
+    {
+      key: "53",
+      name: "Jake White",
+      age: 18,
+      tel: "0575-22098909",
+      phone: 18900010002,
+      address: "Dublin No. 2 Lake Park",
+    },
+    {
+      key: "5",
+      name: "Jake White",
+      age: 18,
+      tel: "0575-22098909",
+      phone: 18900010002,
+      address: "Dublin No. 2 Lake Park",
+    },
+  ];
 
-  //       let totalIndex = pageSize * current;
-  //       totalIndex = totalIndex > data.length ? data.length : totalIndex;
-  //       const fullIndex = pageSize * (current - 1) + index;
-  //       if (index !== 0 && data[fullIndex - 1].tel === data[fullIndex].tel) {
-  //         sameRowCount = 0;
-  //       } else {
-  //         for (let i = fullIndex + 1; i < totalIndex; i++) {
-  //           if (data[i].tel === data[fullIndex].tel) {
-  //             sameRowCount++;
-  //           } else {
-  //             break;
-  //           }
-  //         }
-  //       }
-  //       obj.props.rowSpan = sameRowCount;
-  //       return obj;
-  //     },
-  //   },
-  //   {
-  //     title: "Phone",
-  //     colSpan: 0,
-  //     dataIndex: "phone",
-  //   },
-  //   {
-  //     title: "Address",
-  //     dataIndex: "address",
-  //   },
-  //   {
-  //     title: "操作",
-  //     fixed: "right",
-  //     render: () => {
-  //       return (
-  //         <Space>
-  //           <a>查看</a>
-  //           <a>编辑</a>
-  //           <a style={{ color: "#ed4014" }}>删除</a>
-  //         </Space>
-  //       );
-  //     },
-  //   },
-  // ];
-  // const rowSelection = {
-  //   selectedRowKeys,
-  //   onChange: (newSelectedRowKeys: any) => {
-  //     setSelectedRowKeys(newSelectedRowKeys);
-  //   },
-  //   getCheckboxProps: (record: any) => ({
-  //     disabled: record.name === "Disabled User",
-  //     name: record.name,
-  //   }),
-  //   renderCell(checked: any, record: any, index: number, originNode: any) {
-  //     const { current, pageSize } = pagination || {};
-  //     const obj = {
-  //       children: originNode,
-  //       props: {},
-  //     };
-  //     let sameRowCount = 1;
-  //     let totalIndex = pageSize * current;
-  //     totalIndex = totalIndex > data.length ? data.length : totalIndex;
-  //     const fullIndex = pageSize * (current - 1) + index;
-  //     if (index !== 0 && data[fullIndex - 1].tel === data[fullIndex].tel) {
-  //       sameRowCount = 0;
-  //     } else {
-  //       for (let i = fullIndex + 1; i < totalIndex; i++) {
-  //         if (data[i].tel === data[fullIndex].tel) {
-  //           sameRowCount++;
-  //         } else {
-  //           break;
-  //         }
-  //       }
-  //     }
-  //     obj.props.rowSpan = sameRowCount;
-  //     return obj;
-  //   },
-  // };
-  // 需要合并的字段
-  //合并数组单元格
-  // const createNewArr = (data) => {
-  //   return data
-  //     .reduce((result, item) => {
-  //       //首先将name字段作为新数组result取出
-  //       if (result.indexOf(item.tel) < 0) {
-  //         result.push(item.tel);
-  //       }
-  //       return result;
-  //     }, [])
-  //     .reduce((result, tel) => {
-  //       //将name相同的数据作为新数组取出，并在其内部添加新字段**rowSpan**
-  //       const children = data.filter((item) => item.tel === tel);
-  //       result = result.concat(
-  //         children.map((item, index) => ({
-  //           ...item,
-  //           rowSpan: index === 0 ? children.length : 0, //将第一行数据添加rowSpan字段
-  //         })),
-  //       );
-  //       return result;
-  //     }, []);
-  // };
-  return (
-    <>
-      {/* <SearchForm /> */}
-      <Space style={{ display: "flex", padding: "10px 0" }}>
-        <Space align={"center"}>
-          <div
-            style={{
-              border: "1px solid #abdcff",
-              borderRadius: "6px",
-              padding: "0 10px",
-              background: "#f0faff",
-            }}>
-            <Space>
-              <div>已选择 {selectedRowKeys.length} 项</div>
-            </Space>
-          </div>
-        </Space>
-        <BatchRejectionModal data={selectedRowKeys} selectArray={setSelectedRowKeys} />
-      </Space>
-      <Table
-        columns={columns}
-        dataSource={data}
-        rowKey="id"
-        rowSelection={{
-          onCell: handleOnCell,
-          onSelect: handleOnSelect,
-          onSelectAll: handleOnSelectAll,
-          selectedRowKeys: selectedRowKeys,
-        }}
-        bordered={true}
-      />
-    </>
-  );
+  const handleOnSelect = (record: any, selected: boolean, selectedRows: any) => {};
+  const handleOnSelectAll = (selected: boolean, selectedRows: any, changeRows: any) => {};
+  const handleChange = (pagination: any, filters: any, sorter: any) => {
+    console.log("params", pagination, filters, sorter);
+    setPagination(pagination);
+  };
+  return <Table size="small" onChange={handleChange} columns={columns} dataSource={data} bordered={true} />;
 };
 export default Index;
